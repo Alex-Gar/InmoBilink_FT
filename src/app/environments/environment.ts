637bcc046c356b393a186869329d9e830c00354b
alexgarcia <ale.ag538@gmail.com>
2024-10-20 19:41:27 -0600

    Se configuraron las variables de entorno en los entornos de desarrollo

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    URL_BACKEND: 'http://api.inmobilink.com:8000/',
+    URL_SERVICIOS: 'http://api.inmobilink.com/api',
+    URL_FROTEND: 'http://admin.inmobilink.com:4200',
+};
